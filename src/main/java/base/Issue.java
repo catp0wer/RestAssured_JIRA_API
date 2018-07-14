585ccf1c2decd0e85fb149a53dc6c046fc0ea47c
laila <laila@fake.com>
2018-07-14 23:09:52 +0300

    add a new method for editing an issue and change the AddIssue test to call the method addIssue from ReusableMethods

--- src/main/java/base/Issue.java
+++ src/main/java/base/Issue.java
@@ -9,26 +9,26 @@ import java.io.IOException;
 import static io.restassured.RestAssured.given;
 import files.payLoad;
 
-
 public class Issue {
     @Test
     public void AddIssue() throws IOException {
-
-        //creating issue
-        RestAssured.baseURI = ReusableMethods.getProp("HOST");
-        Response res = given().header("Content-Type","application/json").
-                header("Cookie","JSESSIONID="+ReusableMethods.getSessionID()).
-                body(payLoad.getPostDataCreateIssue()).
-                when().
-                post("/rest/api/2/issue").
-                then().
-                statusCode(201).extract().response();
-        //convert response into String
-        String respString = ReusableMethods.responseToString(res);
-        //convert response from String to Json to extract bug's id from json response
-        JsonPath respJson = ReusableMethods.responseToJson(respString);
-        System.out.println(ReusableMethods.getValueFromResponse(respJson,"id"));
-
+       ReusableMethods.AddIssue();
     }
 
+    @Test
+    public void EditIssue() throws IOException {
+        Response addIssueResponse = ReusableMethods.AddIssue();
+        String issueID = ReusableMethods.getValueFromJson(addIssueResponse,"id");
+
+        RestAssured.baseURI = ReusableMethods.getProp("HOST");
+         given().
+                header("Content-Type", "application/json").
+                header("Cookie", "JSESSIONID=" + ReusableMethods.getSessionID()).
+                body(payLoad.getPutDataUpdateIssue()).
+                when().
+                put("/rest/api/2/issue/"+issueID).
+                then().
+                statusCode(204);
+
+    }
 }
