711e19b41c273e46b13e492470b18b36ce6026dd
catp0wer <catp0wer@mail.ru>
2018-07-16 22:50:53 +0300

    add the method for adding comments

--- src/main/java/files/ReusableMethods.java
+++ src/main/java/files/ReusableMethods.java
@@ -31,11 +31,12 @@ public class ReusableMethods {
                 statusCode(201).extract().response();
         return res;
     }
+
     public static String getSessionID() throws IOException {
         //Create a session
         RestAssured.baseURI = getProp("HOST");
         Response sessionResponse =
-                given().header("Content-Type","application/json").
+                given().header("Content-Type", "application/json").
                         body(getProp("POST_body_authentication")).
                         when().
                         post(getProp("POST_res")).
@@ -45,26 +46,39 @@ public class ReusableMethods {
 
 
         //extracting the session value
-        return getValueFromJson(sessionResponse,getProp("Session_id"));
+        return getValueFromJson(sessionResponse, getProp("Session_id"));
     }
-    public static JsonPath responseToJson(String response){
+
+    public static JsonPath responseToJson(String response) {
         JsonPath res = new JsonPath(response);
         return res;
     }
-   public static String getValueFromJson(Response response, String valueToExtract){
-       String respInString = responseToString(response);
-       JsonPath respInJson = responseToJson(respInString);
-       String value = respInJson.get(valueToExtract);
-       System.out.println(valueToExtract+ " is equal to : " + value);
-       return value;
-   }
 
-   public static String responseToString(Response response){
-       String respInString = response.asString();
-       System.out.println(respInString);
-       return respInString;
+    public static String getValueFromJson(Response response, String valueToExtract) {
+        String respInString = responseToString(response);
+        JsonPath respInJson = responseToJson(respInString);
+        String value = respInJson.get(valueToExtract);
+        System.out.println(valueToExtract+ " is equal to : " + value);
+        return value;
+    }
 
-   }
+    public static String responseToString(Response response) {
+        String respInString = response.asString();
+        System.out.println(respInString);
+        return respInString;
 
+    }
 
+    public static Response addComment() throws IOException {
+        RestAssured.baseURI = ReusableMethods.getProp("HOST");
+        Response commentResponse = given().header("Content-Type", "application/json").
+                header("Cookie", "JSESSIONID=" + ReusableMethods.getSessionID()).
+                body(payLoad.getPostDataAddComment()).
+                when().
+                post("/rest/api/2/issue/" + ReusableMethods.getValueFromJson(ReusableMethods.AddIssue(), "id") + "/comment").
+                then().
+                statusCode(201).extract().response();
+        return commentResponse;
+
+    }
 }
