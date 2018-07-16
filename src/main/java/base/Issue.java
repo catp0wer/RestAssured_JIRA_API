925a0294dbdb5aa6042a811521fcf83c164d500c
catp0wer <catp0wer@mail.ru>
2018-07-16 22:49:52 +0300

    add delete method for an issue

--- src/main/java/base/Issue.java
+++ src/main/java/base/Issue.java
@@ -10,25 +10,56 @@ import static io.restassured.RestAssured.given;
 import files.payLoad;
 
 public class Issue {
-    @Test
+    @Test(testName = "Add an issue")
     public void AddIssue() throws IOException {
-       ReusableMethods.AddIssue();
+        ReusableMethods.AddIssue();
     }
 
-    @Test
+    @Test(testName = "Edit an issue")
     public void EditIssue() throws IOException {
         Response addIssueResponse = ReusableMethods.AddIssue();
-        String issueID = ReusableMethods.getValueFromJson(addIssueResponse,"id");
+        String issueID = ReusableMethods.getValueFromJson(addIssueResponse, "id");
 
         RestAssured.baseURI = ReusableMethods.getProp("HOST");
-         given().
+        given().
                 header("Content-Type", "application/json").
                 header("Cookie", "JSESSIONID=" + ReusableMethods.getSessionID()).
                 body(payLoad.getPutDataUpdateIssue()).
                 when().
-                put("/rest/api/2/issue/"+issueID).
+                put("/rest/api/2/issue/" + issueID).
                 then().
                 statusCode(204);
 
+    }
+
+    @Test(testName = "Delete an issue")
+    public void DeleteIssue() throws IOException {
+        Response addIssueResponse = ReusableMethods.AddIssue();
+        String issueID = ReusableMethods.getValueFromJson(addIssueResponse, "id");
+
+        RestAssured.baseURI = ReusableMethods.getProp("HOST");
+        given().
+                header("Content-Type", "application/json").
+                header("Cookie", "JSESSIONID=" + ReusableMethods.getSessionID()).
+                when().
+                delete("/rest/api/2/issue/" + issueID).
+                then().
+                statusCode(204);
+
+    }
+
+    @Test(testName = "Get an issue")
+    public void GetIssue() throws IOException {
+        Response addIssueResponse = ReusableMethods.AddIssue();
+        String issueID = ReusableMethods.getValueFromJson(addIssueResponse, "id");
+
+        RestAssured.baseURI = ReusableMethods.getProp("HOST");
+        given().
+                header("Content-Type", "application/json").
+                header("Cookie", "JSESSIONID=" + ReusableMethods.getSessionID()).
+                when().
+                get("/rest/api/2/issue/"+issueID).
+                then().
+                statusCode(200);
     }
 }
